4cdf01cb213d3d0eae1c4f9aeefeb846aafed6a1
Muthabuli <36258903+Muthabuli@users.noreply.github.com>
2018-04-09 23:32:44 -0700

    Add files via upload

---
+++ calculator/main.js
@@ -0,0 +1,17 @@
+const buttons = document.querySelectorAll('.btn')
+const screen = document.querySelector('#box')
+let current = 0;
+let previous = 0;
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', buttonPressed)
+
+}
+
+function buttonPressed() {
+
+}
+
+buttons.oclick(){
+  content.innerHTML = content;
+}
